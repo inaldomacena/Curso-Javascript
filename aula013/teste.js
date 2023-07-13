e978b2e00649250f85e4f822ca7f9adda7a6d82d
Inaldo Macena <inaldomacena19@gmail.com>
2023-07-13 00:07:13 -0300

    while e Do while

---
+++ aula013/teste.js
@@ -0,0 +1,15 @@
+//console.log('tudo bem')
+
+//Estrutura de repetição com teste logico no inicio 
+var  cont = 1
+while(cont <= 5){
+    console.log(`passo ${cont}`)
+    cont++
+}
+
+//Estrutura de repetição com teste logico no final
+
+do{
+    console.log(`passo ${cont}`)
+    cont++
+}while (cont <= 6 )
